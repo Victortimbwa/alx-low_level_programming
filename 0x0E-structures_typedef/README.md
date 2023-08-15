7542ed2657c44f4ab8f02afe0881a8e6521e71e6
victortimbwa <victortimbwa@gmail.com>
2023-08-15 23:51:45 +0300

    dog.h

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,2 @@
+This project is about structures, typedef.
+I am expected to learn what structures are, when, why and how to use them
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a dog's basic info
+ * @name: First description
+ * @age: Second description
+ * @owner: Third description
+ * Description : a structure that houses a string and a float
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /*DOG_H*/
