5f7fbf29905e252ebeeadb84e3c10c322a30d41f
victortimbwa <victortimbwa@gmail.com>
2023-08-31 15:34:37 +0300

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,11 +7,11 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t count = 1;
-
+	
 	if (h == NULL)
 	{
 		return (0);
 	}
 	printf("%d\n", h->n); /* prints current node value */
 	return (count + print_listint(h->next));
-}
+} 
